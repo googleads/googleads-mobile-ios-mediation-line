46b52a093efc3c70f018f62d2f1845f23269c39b
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-03-05 11:40:33 -0800

    Updating Line iOS adapter for release version 3.0.1.0
    PiperOrigin-RevId: 879171073

--- Package.swift
+++ Package.swift
@@ -45,13 +45,13 @@ let package = Package(
       name: "LineAdapter",
       url:
         "https://dl.google.com/googleadmobadssdk/mediation/ios/line/LineAdapter-3.0.1.0.zip",
-      checksum: "3e86d04e8cec5f0472818012fc8f5bb0ccaa4d4b1f2290fd7c5bf1d5e0c72f39"
+      checksum: "0450d5e3d38a84e278cca80cfa27b6d22ce914a77e6aa2033229b34533728529"
     ),
     .binaryTarget(
       name: "LineSDK",
       url:
         "https://cdn.fivecdm.com/release-sdk/ios/v3.0.1/FiveAd.framework.zip",
-      checksum: "1d0ca55a377fd6dab304e658f48c2c72d7d620ca38cae4c4a7d8cc3ff0febc8a"
+      checksum: "62fc94398a97de61359178fafaf5eec4f1df566a01d65aab9b4a8d0c0b493619"
     ),
   ]
 )
